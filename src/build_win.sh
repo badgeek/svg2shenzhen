981b5edf4b2cedee40999a0d334d617710b801ea
Budi Prakosa <iyok@deadmediafm.org>
2018-03-30 22:47:01 +0700

    windows build script

---
+++ src/build_win.sh
@@ -0,0 +1,6 @@
+rm -fr *.o
+g++ -c ../common/geometry/*  -lpotrace -L. -L../potrace  -I../include -I../polygon -I../potrace -I. -v -std=c++11 
+g++ -c ../common/math/*  -lpotrace -L. -L../potrace  -I../include -I../polygon -I../potrace -I. -v -std=c++11 
+g++ -c ../polygon/*  -lpotrace -L. -L../potrace  -I../include -I../polygon -I../potrace -I. -v -std=c++11 
+g++ -c main.cpp bitmap2component.cpp lodepng.cpp -lpotrace -L. -L../potrace  -I../include -I../polygon -I../potrace -I. -L ../common/  -I ../common/ -std=c++11 
+g++ -o bitmap2component *.o -lpotrace -L. -L../potrace  -I../include -I../polygon -I../potrace -I. -L ../common/  -I ../common/ -std=c++11 -static-libgcc -static-libstdc++
