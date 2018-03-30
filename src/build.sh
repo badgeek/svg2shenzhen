a592fdae7541710b4b20bba87f7f40744af15301
Budi Prakosa <iyok@deadmediafm.org>
2018-03-30 20:07:33 +0700

    fix linux compile

--- src/build.sh
+++ src/build.sh
@@ -1,6 +1,6 @@
 rm -fr *.o
-g++ -c ../common/geometry/*  -lpotrace -L. -L../potrace  -I../include -I../polygon -I../potrace -I. -v
-g++ -c ../common/math/*  -lpotrace -L. -L../potrace  -I../include -I../polygon -I../potrace -I. -v
-g++ -c ../polygon/*  -lpotrace -L. -L../potrace  -I../include -I../polygon -I../potrace -I. -v
-g++ -c main.cpp bitmap2component.cpp lodepng.cpp -lpotrace -L. -L../potrace  -I../include -I../polygon -I../potrace -I. -L ../common/  -I ../common/
-g++ -o bitmap2component *.o -lpotrace -L. -L../potrace  -I../include -I../polygon -I../potrace -I. -L ../common/  -I ../common/
+g++ -c ../common/geometry/*  -lpotrace -L. -L../potrace  -I../include -I../polygon -I../potrace -I. -v -std=c++11 
+g++ -c ../common/math/*  -lpotrace -L. -L../potrace  -I../include -I../polygon -I../potrace -I. -v -std=c++11 
+g++ -c ../polygon/*  -lpotrace -L. -L../potrace  -I../include -I../polygon -I../potrace -I. -v -std=c++11 
+g++ -c main.cpp bitmap2component.cpp lodepng.cpp -lpotrace -L. -L../potrace  -I../include -I../polygon -I../potrace -I. -L ../common/  -I ../common/ -std=c++11 
+g++ -o bitmap2component *.o -lpotrace -L. -L../potrace  -I../include -I../polygon -I../potrace -I. -L ../common/  -I ../common/ -std=c++11 
