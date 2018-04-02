27db17d52ad9ddd2ed6ee5ce621f06cbf3a9107b
Budi Prakosa <iyok@deadmediafm.org>
2018-04-02 10:04:41 +0700

    add set drill size menu

---
+++ inkscape/drill_size.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python 
+
+import inkex
+
+class MyEffect(inkex.Effect):
+    def __init__(self):
+        inkex.Effect.__init__(self)
+        self.OptionParser.add_option("-d", "--drillsize",
+                        action="store", type="float", 
+                        dest="drillsize", default=0.8001,
+                        help="Drill Size")
+    def effect(self):
+        for id, node in self.selected.iteritems():
+            if node.tag == inkex.addNS('circle','svg'):
+                inkex.debug(node)
+                node.set('drill', str(self.options.drillsize))
+                node.set('r', str(self.options.drillsize))
+
+if __name__ == '__main__':
+    e = MyEffect()
+    e.affect()
