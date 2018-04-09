c3d1a0433cbf7c832907149c40d73547d95fb66e
Budi Prakosa <iyok@deadmediafm.org>
2018-04-09 08:46:37 +0700

    windows pthread fix

--- src/build_win.sh
+++ src/build_win.sh
@@ -3,4 +3,4 @@ g++ -c ../common/geometry/*  -lpotrace -L. -L../potrace  -I../include -I../polyg
 g++ -c ../common/math/*  -lpotrace -L. -L../potrace  -I../include -I../polygon -I../potrace -I. -v -std=c++11 
 g++ -c ../polygon/*  -lpotrace -L. -L../potrace  -I../include -I../polygon -I../potrace -I. -v -std=c++11 
 g++ -c main.cpp bitmap2component.cpp lodepng.cpp -lpotrace -L. -L../potrace  -I../include -I../polygon -I../potrace -I. -L ../common/  -I ../common/ -std=c++11 
-g++ -o bitmap2component *.o -lpotrace -L. -L../potrace  -I../include -I../polygon -I../potrace -I. -L ../common/  -I ../common/ -std=c++11 -static-libgcc -static-libstdc++
+g++ -o bitmap2component *.o -lpotrace -L. -L../potrace  -I../include -I../polygon -I../potrace -I. -L ../common/  -I ../common/ -std=c++11 -static-libgcc -static-libstdc++ -static
