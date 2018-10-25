b7c996ef7962712435d5cbe9ea49a95844940ab3
Budi Prakosa <iyok@deadmediafm.org>
2018-10-25 07:15:53 +0700

    finish build script

--- build.sh
+++ build.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
+
 cwd=$(pwd)
+
 BUILD_DIR=dist
+
+BUILD_DIR_STAGING=staging
+
 GIT_TAG_VERSION=`git describe --tag`
-mkdir -p $cwd/$BUILD_DIR
+
+RELEASE_FILENAME_PREFIX=svg2shenzhen
+# echo "$cwd/$BUILD_DIR/$BUILD_DIR_STAGING"
+
 rm -fr $cwd/$BUILD_DIR/*
-cp -r inkscape/* $cwd/$BUILD_DIR
-find $cwd/$BUILD_DIR -name *.inx -type f -exec sed -i.bak s/SVGSZ_VER/${GIT_TAG_VERSION}/g '{}' \;
-rm -fr $cwd/$BUILD_DIR/*.bak
+
+mkdir -p "$cwd/$BUILD_DIR/$BUILD_DIR_STAGING"
+
+cp -r inkscape/* $cwd/$BUILD_DIR/$BUILD_DIR_STAGING
+
+find $cwd/$BUILD_DIR/$BUILD_DIR_STAGING -name *.inx -type f -exec sed -i.bak s/SVGSZ_VER/${GIT_TAG_VERSION}/g '{}' \;
+
+rm -fr $cwd/$BUILD_DIR/$BUILD_DIR_STAGING/*.bak
+
+cd $cwd/$BUILD_DIR/$BUILD_DIR_STAGING
+
+zip -m -x .DS_Store -r $cwd/$BUILD_DIR/${RELEASE_FILENAME_PREFIX}-${GIT_TAG_VERSION}.zip .
