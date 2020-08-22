79422e86f0f45eacadad5db155fdba7eaf1cee62
hoijui <hoijui.quaero@gmail.com>
2020-08-22 10:31:53 +0200

    build.sh: Make more robust & cleanup

--- build.sh
+++ build.sh
@@ -1,31 +1,46 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# Creates distribution archives.
+# Requires the binaries be already built (with make)
+# and moved to the stageing area.
 
-cwd=$(pwd)
+# Exit immediately on each error and unset variable;
+# see: https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+#set -Eeuo pipefail
+set -Eeu
+
+# shellcheck source=./
+script_dir=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
+root="$script_dir"
+cwd="$(pwd)"
 
 BUILD_DIR=dist
 
 BUILD_DIR_STAGING=staging
 
-GIT_TAG_VERSION=`git describe --tag`
+GIT_TAG_VERSION="$(git describe --tag)"
 
 RELEASE_FILENAME_PREFIX=svg2shenzhen-extension
-# echo "$cwd/$BUILD_DIR/$BUILD_DIR_STAGING"
+# echo "$root/$BUILD_DIR/$BUILD_DIR_STAGING"
+RELEASE_FILENAME_BASE="${root:?}/$BUILD_DIR/${RELEASE_FILENAME_PREFIX}-${GIT_TAG_VERSION}"
 
-rm -fr $cwd/$BUILD_DIR/*
+rm -fr "${root:?}/$BUILD_DIR/"*
 
-mkdir -p "$cwd/$BUILD_DIR/$BUILD_DIR_STAGING"
+mkdir -p "${root:?}/$BUILD_DIR/$BUILD_DIR_STAGING"
 
-cp -r inkscape/* $cwd/$BUILD_DIR/$BUILD_DIR_STAGING
+cp -r inkscape/* "${root:?}/$BUILD_DIR/$BUILD_DIR_STAGING"
 
-find $cwd/$BUILD_DIR/$BUILD_DIR_STAGING -name *.inx -type f -exec sed -i.bak s/SVGSZ_VER/${GIT_TAG_VERSION}/g '{}' \;
+find "${root:?}/$BUILD_DIR/$BUILD_DIR_STAGING" \
+	-name "*.inx" \
+	-type f \
+	-exec sed -i.bak "s/SVGSZ_VER/${GIT_TAG_VERSION}/g" '{}' \;
 
-rm -fr $cwd/$BUILD_DIR/$BUILD_DIR_STAGING/*.bak
+rm -fr "${root:?}/$BUILD_DIR/$BUILD_DIR_STAGING/"*.bak
 
-cd $cwd/$BUILD_DIR/$BUILD_DIR_STAGING
+cd "${root:?}/$BUILD_DIR/$BUILD_DIR_STAGING"
 
-tar -czvf $cwd/$BUILD_DIR/${RELEASE_FILENAME_PREFIX}-${GIT_TAG_VERSION}.tar.gz .
-zip -m -x .DS_Store -r $cwd/$BUILD_DIR/${RELEASE_FILENAME_PREFIX}-${GIT_TAG_VERSION}.zip .
+tar -czvf "${RELEASE_FILENAME_BASE}.tar.gz" .
+zip -m -x .DS_Store -r "${RELEASE_FILENAME_BASE}.zip" .
 
-cd $cwd
+cd "$cwd"
 
-ls dist
+ls "$BUILD_DIR"
